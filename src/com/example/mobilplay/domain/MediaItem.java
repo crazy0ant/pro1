91a42d5cc874d354ff9eeb97cf0cf0d0939ba368
361763560@qq.com <361dch1120>
2018-06-17 15:44:42 +0800

    domain

---
+++ src/com/example/mobilplay/domain/MediaItem.java
@@ -0,0 +1,67 @@
+package com.example.mobilplay.domain;
+
+/*
+ * 代表一个视频和音频
+ */
+public class MediaItem {
+	
+	private String name; 
+	
+	private Long duration;
+	
+	private Long size;
+	
+	private String data;
+	
+	private String artist;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Long duration) {
+		this.duration = duration;
+	}
+
+	public Long getSize() {
+		return size;
+	}
+
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	@Override
+	public String toString() {
+		return "MediaItem [name=" + name + ", duration=" + duration + ", size="
+				+ size + ", data=" + data + ", artist=" + artist + "]";
+	}
+	
+	
+	
+	
+}
